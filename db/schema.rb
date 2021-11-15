ea829e5811a1f7518c29afc7fbd7dcd938d802db
richardleebr <richardlee@bleacherreport.com>
2021-11-15 02:09:10 -0800

    Run rushing stats columns migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_093938) do
+ActiveRecord::Schema.define(version: 2021_11_15_100013) do
 
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "team"
+    t.string "position"
+    t.integer "rushing_attempts"
+    t.decimal "rushing_attempts_per_game"
+    t.integer "rushing_yards"
+    t.decimal "rushing_avg"
+    t.decimal "rushing_yards_per_game"
+    t.integer "rushing_touchdowns"
+    t.integer "rushing_long"
+    t.boolean "rushing_long_touchdown_occured"
+    t.integer "rushing_1st_downs"
+    t.decimal "rushing_1st_down_percentage"
+    t.integer "rushing_20_yards_plus_plays"
+    t.integer "rushing_40_yards_plus_plays"
+    t.integer "fumbles"
   end
 
 end
